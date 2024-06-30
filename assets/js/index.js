a5dd66950554279127b6e6a48bd40dbc6f7d4b7b
ielmira <elmirakarimpour69@gmail.com>
2024-06-30 14:37:05 +0330

    add task in list in html and js

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,31 @@ toggleBtn.addEventListener("change", () => {
 
   console.log("ok");
 });
+
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+function addTask () {
+    const taskText = taskInput.value.trim();
+    if (taskText !== ''){
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'delete';
+    deleteBtn.addEventListener('click' , deleteTask);
+    li.appendChild(deleteBtn);
+}
+
+function completeTask (event) {
+    const task = event.target;
+    task.classList.toggle('completed')
+}
+li.addEventListener('click' , completeTask);
+
+function deleteTask (event){
+    const task = event.target.parentElement;
+    taskList.removeChild(task);
+}
